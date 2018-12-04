fcf1d99414ae919bef4ddcff3dbe23129940c140
Michael Karner <mk@digitalis.at>
2018-12-04 17:21:24 +0100

    index added

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Web Test</title>
+    </head>
+
+    <body>
+        <h2>Hallo Welt</h2><br/>
+        <?php echo("PHP:> Das ist ein Test"); ?> <br/>
+        <i>Und aus...</i>
+
+    </body>
+
+</html>
